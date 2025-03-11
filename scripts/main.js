111fd49069f60a1fd50e57b2b909495ea6f83ddc
Leo Nagano <leonagano@gmail.com>
2025-03-11 10:43:27 -0300

    new project

--- scripts/main.js
+++ scripts/main.js
@@ -812,8 +812,7 @@ function centerMapOnUser(username) {
         }
         
         // Center the camera on the user's properties
-        console.log("Camera position", camera.position);
-        camera.position.set(userPosition.x, 5, userPosition.z + 10);
+        camera.position.set(userPosition.x - 50, 5, userPosition.z + 10);
         camera.lookAt(userPosition.x, 0, userPosition.z);
     } else {
         console.warn(`No properties found for user: ${username}`); // Debug log
